d48bb70bdd22a338c49807b2a636f1048cc34175
Rene Ruano <rene.ruano95@gmail.com>
2024-06-20 22:18:03 -0400

    chore: initial setup, add drizzle orm

---
+++ drizzle.config.ts
@@ -0,0 +1,18 @@
+import { config } from "dotenv";
+import { defineConfig } from "drizzle-kit";
+
+config({ path: ".env" });
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("🔴 Cannot find database url");
+}
+
+export default defineConfig({
+  dialect: "postgresql",
+  schema: "./src/lib/supabase/schema.ts",
+  out: "./migrations",
+  dbCredentials: {
+    url: process.env.DATABASE_URL, // Supabase url from .env
+  },
+  schemaFilter: ["public"],
+});

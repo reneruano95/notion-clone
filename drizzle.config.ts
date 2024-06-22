38ec0d3a4a4198f3b4267c32bf3166e9410c88bb
Rene Ruano <rene.ruano95@gmail.com>
2024-06-21 22:49:47 -0400

    feat: pull stripe subscriptions schemas from supabase

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -12,6 +12,11 @@ export default defineConfig({
   schema: "./lib/supabase/schema.ts",
   out: "./migrations",
   dbCredentials: {
-    url: process.env.DATABASE_URL || "", // Supabase url from .env
+    url: process.env.DATABASE_URL || "",
+    database: "postgres",
+    port: 5432,
+    host: "aws-0-us-east-1.pooler.supabase.com",
+    user: "postgres.fdhollsltcvmcaicpajz",
+    password: process.env.PW || "",
   },
 });
--- lib/supabase/schema.ts
+++ lib/supabase/schema.ts
@@ -1,4 +1,13 @@
-import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import {
+  boolean,
+  integer,
+  jsonb,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+} from "drizzle-orm/pg-core";
+import { prices, subscription_status, users } from "@/migrations/schema";
 
 export const workspaces = pgTable("workspaces", {
   id: uuid("id").defaultRandom().primaryKey().notNull(),
@@ -17,7 +26,7 @@ export const workspaces = pgTable("workspaces", {
   bannerUrl: text("banner_url").notNull(),
 });
 
-const folders = pgTable("folders", {
+export const folders = pgTable("folders", {
   id: uuid("id").defaultRandom().primaryKey().notNull(),
   createdAt: timestamp("created_at", {
     mode: "string",
@@ -29,14 +38,15 @@ const folders = pgTable("folders", {
   iconId: uuid("icon_id").notNull(),
   data: text("data").notNull(),
   inTrash: text("in_trash").notNull(),
-  logo: text("logo").notNull(),
   bannerUrl: text("banner_url").notNull(),
-  workspaceId: uuid("workspace_id").references(() => workspaces.id, {
-    onDelete: "cascade",
-  }),
+  workspaceId: uuid("workspace_id")
+    .notNull()
+    .references(() => workspaces.id, {
+      onDelete: "cascade",
+    }),
 });
 
-const files = pgTable("files", {
+export const files = pgTable("files", {
   id: uuid("id").defaultRandom().primaryKey().notNull(),
   createdAt: timestamp("created_at", {
     mode: "string",
@@ -48,12 +58,62 @@ const files = pgTable("files", {
   iconId: uuid("icon_id").notNull(),
   data: text("data").notNull(),
   inTrash: text("in_trash").notNull(),
-  logo: text("logo").notNull(),
   bannerUrl: text("banner_url").notNull(),
-  workspaceId: uuid("workspace_id").references(() => workspaces.id, {
-    onDelete: "cascade",
-  }),
-  folderId: uuid("folder_id").references(() => folders.id, {
-    onDelete: "cascade",
-  }),
+  workspaceId: uuid("workspace_id")
+    .notNull()
+    .references(() => workspaces.id, {
+      onDelete: "cascade",
+    }),
+  folderId: uuid("folder_id")
+    .notNull()
+    .references(() => folders.id, {
+      onDelete: "cascade",
+    }),
+});
+
+export const subscriptions = pgTable("subscriptions", {
+  id: text("id").primaryKey().notNull(),
+  user_id: uuid("user_id")
+    .notNull()
+    .references(() => users.id),
+  status: subscription_status("status"),
+  metadata: jsonb("metadata"),
+  price_id: text("price_id").references(() => prices.id),
+  quantity: integer("quantity"),
+  cancel_at_period_end: boolean("cancel_at_period_end"),
+  created: timestamp("created", { withTimezone: true, mode: "string" })
+    .defaultNow()
+    .notNull(),
+  current_period_start: timestamp("current_period_start", {
+    withTimezone: true,
+    mode: "string",
+  })
+    .defaultNow()
+    .notNull(),
+  current_period_end: timestamp("current_period_end", {
+    withTimezone: true,
+    mode: "string",
+  })
+    .defaultNow()
+    .notNull(),
+  ended_at: timestamp("ended_at", {
+    withTimezone: true,
+    mode: "string",
+  }).defaultNow(),
+  cancel_at: timestamp("cancel_at", {
+    withTimezone: true,
+    mode: "string",
+  }).defaultNow(),
+  canceled_at: timestamp("canceled_at", {
+    withTimezone: true,
+    mode: "string",
+  }).defaultNow(),
+  trial_start: timestamp("trial_start", {
+    withTimezone: true,
+    mode: "string",
+  }).defaultNow(),
+  trial_end: timestamp("trial_end", {
+    withTimezone: true,
+    mode: "string",
+  }).defaultNow(),
 });

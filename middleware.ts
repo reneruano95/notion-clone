2afaaa200bd3e8b94bd891fef86775f7d56a4963
Rene Ruano <rene.ruano95@gmail.com>
2024-06-25 20:37:26 -0400

    feat: add supabase integration

---
+++ middleware.ts
@@ -0,0 +1,33 @@
+import { NextResponse, type NextRequest } from "next/server";
+import { updateSession } from "@/lib/supabase/middleware";
+
+export async function middleware(request: NextRequest) {
+  const { response, supabase } = await updateSession(request);
+
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser();
+
+  if (
+    !user &&
+    !request.nextUrl.pathname.startsWith("/sign") &&
+    !request.nextUrl.pathname.startsWith("/")
+  ) {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+  }
+
+  return response;
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
+};

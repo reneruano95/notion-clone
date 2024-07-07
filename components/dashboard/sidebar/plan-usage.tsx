9f595eab2f254baae5b41ebc1c7df365222fbac7
Rene Ruano <rene.ruano95@gmail.com>
2024-07-07 19:06:13 -0400

    feat: Implement plan usage component

--- components/dashboard/sidebar/plan-usage.tsx
+++ components/dashboard/sidebar/plan-usage.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import { useEffect, useMemo, useState } from "react";
-import { usePathname } from "next/navigation";
 
 import { Tables } from "@/lib/supabase/supabase.types";
 import { MAX_FOLDERS_FREE_PLAN } from "@/lib/constants";
+import { useAppsStore } from "@/lib/providers/store-provider";
+import { Progress } from "@/components/ui/progress";
+import CypressDiamondIcon from "@/components/icons/cypressDiamongIcon";
 
 interface PlanUsageProps {
   foldersLength: number;
@@ -12,23 +14,36 @@ interface PlanUsageProps {
 }
 
 export const PlanUsage = ({ foldersLength, subscription }: PlanUsageProps) => {
-  const pathname = usePathname();
+  const { folders } = useAppsStore((store) => store);
+
   const [usagePercentage, setUsagePercentage] = useState(
     (foldersLength / MAX_FOLDERS_FREE_PLAN) * 100
   );
 
   useEffect(() => {
-    const stateFoldersLength = foldersLength;
+    const stateFoldersLength = folders?.length;
+
+    if (stateFoldersLength === undefined) return;
+
     setUsagePercentage((stateFoldersLength / MAX_FOLDERS_FREE_PLAN) * 100);
-  }, [foldersLength, subscription]);
+  }, [folders]);
 
-  const workspaceId = useMemo(() => {
-    const urlSegments = pathname?.split("/").filter(Boolean);
-    if (urlSegments)
-      if (urlSegments.length > 1) {
-        return urlSegments[1];
-      }
-  }, [pathname]);
-
-  return <div>PlanUsage</div>;
+  return (
+    <article className="mb-4">
+      {subscription?.status !== "active" && (
+        <div className="flex  gap-2 text-muted-foreground mb-2 items-center">
+          <div className="h-4 w-4">
+            <CypressDiamondIcon />
+          </div>
+          <div className="flex justify-between w-full items-center">
+            <div>Free Plan</div>
+            <small>{usagePercentage.toFixed(0)}% / 100%</small>
+          </div>
+        </div>
+      )}
+      {subscription?.status !== "active" && (
+        <Progress value={usagePercentage} className="h-1" />
+      )}
+    </article>
+  );
 };

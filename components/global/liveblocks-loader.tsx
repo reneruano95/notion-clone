906ec657dab8f1c9436246ee829953b8ac481bfa
Rene Ruano <rene.ruano95@gmail.com>
2024-07-27 05:05:31 -0400

    refactor: Remove redundant CSS class in LiveblocksLoader component

--- components/global/liveblocks-loader.tsx
+++ components/global/liveblocks-loader.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export const LiveblocksLoader = () => {
   return (
-    <div className="fixed top-0 left-0 z-50 flex items-center justify-center w-screen h-screen bg-transparent">
+    <div className="z-50 flex items-center justify-center w-screen h-screen bg-transparent">
       <div className="flex items-center justify-center space-x-3">
         <Image
           src="/liveblocks-loader.svg"

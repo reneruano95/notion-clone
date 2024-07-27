4c402c6d82b176912ef1efecc5f3a8c33569ada6
Rene Ruano <rene.ruano95@gmail.com>
2024-07-27 13:49:29 -0400

    refactor: Add tooltip functionality to display collaborator names in ActiveCollaborators component

--- components/liveblocks/active-collaborators.tsx
+++ components/liveblocks/active-collaborators.tsx
@@ -1,5 +1,12 @@
 import { useOthers, useSelf } from "@liveblocks/react/suspense";
+
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "../ui/tooltip";
 
 export const ActiveCollaborators = () => {
   const others = useOthers();
@@ -12,25 +19,43 @@ export const ActiveCollaborators = () => {
     <ul className="flex px-3">
       {collaborators.map(({ id, avatar, name, color }) => (
         <li key={id} className="relative">
-          <Avatar
-            className="inline-block size-8 ring-2"
-            style={{ border: `2px solid ${color}` }}
-          >
-            <AvatarImage src={avatar || ""} />
-            <AvatarFallback>{name[0]}</AvatarFallback>
-          </Avatar>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Avatar
+                  className="inline-block size-8 ring-2"
+                  style={{ border: `2px solid ${color}` }}
+                >
+                  <AvatarImage src={avatar || ""} />
+                  <AvatarFallback className="select-none">
+                    {name[0]}
+                  </AvatarFallback>
+                </Avatar>
+              </TooltipTrigger>
+              <TooltipContent>{name}</TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
         </li>
       ))}
 
       {currentUser && (
         <li className="relative">
-          <Avatar
-            className="inline-block size-8 ring-2"
-            style={{ border: `2px solid ${currentUser.info.color}` }}
-          >
-            <AvatarImage src={currentUser.info.avatar || ""} />
-            <AvatarFallback>{currentUser.info.name[0]}</AvatarFallback>
-          </Avatar>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Avatar
+                  className="inline-block size-8 ring-2"
+                  style={{ border: `2px solid ${currentUser.info.color}` }}
+                >
+                  <AvatarImage src={currentUser.info.avatar || ""} />
+                  <AvatarFallback className="select-none">
+                    {currentUser.info.name[0]}
+                  </AvatarFallback>
+                </Avatar>
+              </TooltipTrigger>
+              <TooltipContent>{currentUser.info.name}</TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
         </li>
       )}
     </ul>
